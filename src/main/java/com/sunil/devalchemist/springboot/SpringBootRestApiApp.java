3faf18ee568c67af4e67d020ffe0318751433d1f
SunilNekkanti <SunilNekkanti@users.noreply.github.com>
2017-02-11 15:03:08 -0700

    changed a lot 1234568877777

--- src/main/java/com/sunil/devalchemist/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/sunil/devalchemist/springboot/SpringBootRestApiApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.sunil.devalchemist.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
 public class SpringBootRestApiApp {
-//changed a lot
+//changed a lot 1234568877777
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
 	}
