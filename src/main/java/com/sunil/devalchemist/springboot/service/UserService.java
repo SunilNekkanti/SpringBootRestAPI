f6f8685f264143f1bba1a8784d12bff4618e739f
SUNILS <venky.sunil@gmail.com>
2017-02-11 09:19:02 -0700

    Initial Commit

---
+++ src/main/java/com/sunil/devalchemist/springboot/service/UserService.java
@@ -0,0 +1,26 @@
+package com.sunil.devalchemist.springboot.service;
+
+
+import java.util.List;
+
+import com.sunil.devalchemist.springboot.model.User;
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers();
+	
+	void deleteAllUsers();
+	
+	boolean isUserExist(User user);
+	
+}
---
+++ src/main/java/com/sunil/devalchemist/springboot/util/CustomErrorType.java
@@ -0,0 +1,16 @@
+package com.sunil.devalchemist.springboot.util;
+
+
+public class CustomErrorType {
+
+    private String errorMessage;
+
+    public CustomErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
