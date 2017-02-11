12f4367cffebdf09865fdf3a66b9823a7f76788c
SunilNekkanti <SunilNekkanti@users.noreply.github.com>
2017-02-11 15:05:55 -0700

    9848022338

--- src/main/java/com/sunil/devalchemist/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/sunil/devalchemist/springboot/SpringBootRestApiApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.sunil.devalchemist.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
 public class SpringBootRestApiApp {
-//changed a lot 1234568877777
+//9848022338
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
 	}
